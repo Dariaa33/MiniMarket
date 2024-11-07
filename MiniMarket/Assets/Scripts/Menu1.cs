71104602ea64f44852779ce210c4a66352bf0ba4
Dariaa33 <vesnayoon@gmail.com>
2024-11-07 12:17:53 +0000

    MiniMarket
    UI sdelano, nado napisat´scripty k menu

---
+++ MiniMarket/Assets/Scripts/Menu1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu1 : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
