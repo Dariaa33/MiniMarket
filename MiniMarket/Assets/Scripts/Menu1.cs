1436483525a8925cbd7662365174427b49cdd840
Dariaa33 <vesnayoon@gmail.com>
2024-11-08 09:51:23 +0000

    MiniMarket
    sdelany knopki otkryt´y vrode zakryt

--- MiniMarket/Assets/Scripts/Menu1.cs
+++ MiniMarket/Assets/Scripts/Menu1.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Menu1 : MonoBehaviour
 {
-    
+    [SerializeField]
+    GameObject menu;
+    float timeAnim;
     void Start()
     {
-        
+        menu.SetActive(true);
     }
 
     
     void Update()
-    {
+    { 
+       
+      
+        /* if(menu.activeSelf)
+        {
+            ShowMenu();
+        }*/
+        
         
     }
+
+    public void ShowMenu()
+    {
+        menu.SetActive(true);
+        LeanTween.moveLocalY(menu, -261f, 0f);
+        LeanTween.moveLocalY(menu, -261f, timeAnim).setEase(LeanTweenType.animationCurve);
+        /*LeanTween.moveLocalY(menu, -1100f, 0f);
+        LeanTween.moveLocalY(menu, 0f, timeAnim).setEase(LeanTweenType.animationCurve);*/
+    }
+
+    public void HideMenu()
+    {
+        menu.SetActive(true);
+        LeanTween.moveLocalY(menu, -322f, 0f);
+        LeanTween.moveLocalY(menu, -322f, timeAnim).setEase(LeanTweenType.animationCurve);
+    }
 }
+
